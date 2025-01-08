e75db8089e93dfb13d5bbc14454be3b980f9e900
zhuwenxing <wenxing.zhu@zilliz.com>
2025-01-08 14:05:08 +0800

    fix ruff check and version
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

--- src/milvus_dataset/__init__.py
+++ src/milvus_dataset/__init__.py
@@ -1,7 +1,7 @@
+from .__version__ import __version__
 from .core import ConfigManager, Dataset, DatasetDict, list_datasets, load_dataset
 from .log_config import configure_logger, logger
 from .storage import StorageConfig, StorageType
-from .__version__ import __version__
 
 __all__ = [
     "ConfigManager",
@@ -9,9 +9,9 @@ __all__ = [
     "DatasetDict",
     "StorageConfig",
     "StorageType",
+    "__version__",
     "configure_logger",
     "list_datasets",
     "load_dataset",
     "logger",
-    "__version__",
 ]
